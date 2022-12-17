6644cd4862595d836b89cb03dd90047f1225af61
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-12-17 04:18:56 +0300

    create header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,9 @@ import { useState } from "react"
 import bars from '../../assets/bars.svg'
 import cross from '../../assets/cross.svg'
 import logo from '../../assets/Logo.svg'
+import RightItem from "./RightItems/RightItem"
+import mapMarker from '../../assets/map-marker.svg'
+import phone from '../../assets/phone.svg'
 
 export default function Header() {
     const [toggle, setToggle] = useState(false)
@@ -33,9 +36,23 @@ export default function Header() {
                     <li className="menu__item"><a className="anchor header_menu_anchor" href="#">Расчет стоимости</a></li>
                     <li className="menu__item"><a className="anchor header_menu_anchor" href="#">Партнеры</a></li>
                     <li className="menu__item"><a className="anchor header_menu_anchor" href="#">Отзывы</a></li>
-
-                    <li className="menu__item"><a className="header_menu_anchor" href="#">Партнеры</a></li>
-                    <li className="menu__item"><a className="header_menu_anchor" href="#">Отзывы</a></li>
+                    
+                    <li className="menu__item menu_right_items">
+                            <RightItem className='r__item'
+                                img={mapMarker}
+                                alt='город'
+                                boldTxt={'Москва'}
+                                normalTxt={'Все города'}
+                                isCity={true}
+                            />
+                            <RightItem className='r_item'
+                                img={phone}
+                                alt='телефон'
+                                boldTxt={'8-985-344-76-46'}
+                                normalTxt={'Заказать звонок'}
+                                isCity={false}
+                            />
+                    </li>
                 </ul>
             </nav>
         </header>
--- src/components/header/RightItems/RightItem.jsx
+++ src/components/header/RightItems/RightItem.jsx
@@ -1,5 +1,13 @@
-export default function RightHeaderBlocks () {
+export default function RightItem ({img, alt, boldTxt, normalTxt, isCity}) {
     return (
-        <h1></h1>
+        <div className="r_item_container">
+            <div className="r_item_top">
+                <img src={img} alt={alt} className="r_item_img" />
+                <div className="r_item_texts">
+                    <a className="r_item_bold">{boldTxt}</a>
+                    <a href="#" className={isCity ? 'r_item_anchor_city r_item_anchor' : 'r_item_anchor_phone r_item_anchor'}>{normalTxt}</a>
+                </div>
+            </div>
+        </div>
     )
 }
