255bbe97bdbbe6a32f41afd2416c89b146c5f0d6
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-12-22 06:51:26 +0300

    almost create advantages block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./header/Header"
 import Banner from "./banner/Banner"
+import Advanatages from "./advantages/Advantages"
 
 function App() {
    return (
     <div className="App">
       <Header />
       <Banner />
+      <Advanatages />
     </div>
   )
 }
--- src/components/advantages/Advantages.jsx
+++ src/components/advantages/Advantages.jsx
@@ -1,5 +1,74 @@
+import img1 from '../../assets/advantageImg1.svg'
+import img2 from '../../assets/piggy.svg'
+import img3 from '../../assets/cycle.svg'
+import img4 from '../../assets/montage.svg'
+
 export default function Advanatages () {
     return (
-        <h1></h1>
+        <div className="advantages__body">
+            <h2 className="heading advantages__heading">Преимущества системы</h2>
+            <div className="advantages__container">
+                <div className="advantage">
+                    <div className="advantage_img_container">
+                        <img src={img1} alt="изображение 1" className="advantage__image" />
+                    </div>
+                    <div className="advantage__txts">
+                        <h3 className="advantage__heading">Высокая прочность,
+                            жёсткость и надёжность</h3>
+                        <h4 className="advantage__description">По прочности  соединение с помощью 
+                            краб-системы ничем не уступает сварке</h4>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <div className="advantage_img_container">
+                        <img src={img2} alt="изображение 1" className="advantage__image" />
+                    </div>
+                    <div className="advantage__txts">
+                        <h3 className="advantage__heading">Низкая стоимость</h3>
+                        <h4 className="advantage__description">Цена конструкции относительна низка, так как: 
+                        <br />
+                            • Вам не нужно использовать сварочный аппарат; <br />
+                            • работу Вы можете сделать своими руками</h4>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <div className="advantage_img_container">
+                        <img src={img3} alt="изображение 1" className="advantage__image" />
+                    </div>
+                    <div className="advantage__txts">
+                        <h3 className="advantage__heading">Возможность многоразового 
+                            использования</h3>
+                        <h4 className="advantage__description">Система соединения позволяет перестроить 
+                            конструкцию – уменьшить, увеличить размеры</h4>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <div className="advantage_img_container">
+                        <img src={img4} alt="изображение 1" className="advantage__image" />
+                    </div>
+                    <div className="advantage__txts">
+                        <h3 className="advantage__heading">Простота монтажа</h3>
+                        <h4 className="advantage__description">Для выполнения работ Вам не требуется 
+                            специальных знаний, Краб-система 
+                            закрепляется при помощи болтов и гаек </h4>
+                    </div>
+                </div>
+
+                <div className="advantage">
+                    <div className="advantage_img_container">
+                        <img src={img1} alt="изображение 1" className="advantage__image" />
+                    </div>
+                    <div className="advantage__txts">
+                        <h3 className="advantage__heading">Высокая прочность,
+                            жёсткость и надёжность</h3>
+                        <h4 className="advantage__description">По прочности  соединение с помощью 
+                            краб-системы ничем не уступает сварке</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
