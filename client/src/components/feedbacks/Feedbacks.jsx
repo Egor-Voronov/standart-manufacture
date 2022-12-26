0e0b354cfea532ad86911b183274b8ebaa655a43
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-12-26 22:21:58 +0300

    almost create feedback api

--- client/src/components/feedbacks/Feedbacks.jsx
+++ client/src/components/feedbacks/Feedbacks.jsx
@@ -1,7 +1,13 @@
-import Review from "./feedback/Feedback";
-import Data from "./test/Data";
+import Feedback from "./feedback/Feedback";
+import { useEffect, useState, CSSProperties } from "react";
+import axios from "axios";
+import { useFetching } from "../../hooks/useFetch.hook";
+import ClipLoader from "react-spinners/ClipLoader"
 
 export default function Feedbacks() {
+  const [data, error, isLoading] = useFetching('http://127.0.0.1:8000/api/feedbacks/')
+  console.log(data)
+
   return (
     <div className="reviews__body" id="reviews">
       <h2 className="heading reviews__heading mobile__heading">
@@ -11,9 +17,26 @@ export default function Feedbacks() {
         отзывы наших клиентов
       </h2>
       <div className="reviews__container">
-        {Data.map((elem) => (
+        {/* {Data.map((elem) => (
           <Review key={elem.id} {...elem} />
-        ))}
+        ))} */}
+
+      {
+        isLoading?(
+          <ClipLoader />
+        ): error?(
+          <h1>{error}</h1>
+        ): data.length? (
+          <>{
+            data.map(e => (
+              <Feedback key={e.id} {...e} />
+            ))
+          }</>
+        ): (
+          <h1>нет данных</h1>
+        )
+      }
+
       </div>
     </div>
   );
--- client/src/components/feedbacks/feedback/Feedback.jsx
+++ client/src/components/feedbacks/feedback/Feedback.jsx
@@ -7,8 +7,8 @@ export default function Feedback (props) {
         <img src={client} alt="фото клиента" className="review__img" />
       </div>
       <div className="review__txts">
-        <h3 className="review__name">{props.name}</h3>
-        <p className="review__text">{props.review}</p>
+        <h3 className="review__name">{props.author_name.toString()}</h3>
+        <p className="review__text">{props.text}</p>
       </div>
     </div>
   );
