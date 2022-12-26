57fab9421319d28e118b16500cb29fc56f8c4f40
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-12-27 02:19:35 +0300

    client fixes

--- client/src/components/advantages/Advantages.jsx
+++ client/src/components/advantages/Advantages.jsx
@@ -1,7 +1,12 @@
-import img1 from "../../assets/2802966240624.svg";
-import img2 from "../../assets/truck.svg";
-import img3 from "../../assets/rub.svg";
-import img4 from "../../assets/like.svg";
+import img1 from "../../assets/advantageImg1.svg";
+import img2 from "../../assets/piggy.svg";
+import img3 from "../../assets/cycle.svg";
+import img4 from "../../assets/montage.svg";
+
+import img5 from "../../assets/2802966240624.svg";
+import img6 from "../../assets/truck.svg";
+import img7 from "../../assets/rub.svg";
+import img8 from "../../assets/like.svg";
 
 import Advantage from "./advantage/Advantage";
 
@@ -13,26 +18,26 @@ export default function Advanatages2({isSecondAdvantage}) {
         {isSecondAdvantage ?(
           <>
         <Advantage
-          img={img1}
+          img={img5}
           heading="Собственное производство"
           description="Мы изготавливаем элементы краб-системы на собственном производстве в Вологде, уделяя особое внимание качеству."
         />
 
         <Advantage
-          img={img2}
+          img={img6}
           heading="Доставка по всей России"
           description="Мы осуществляем доставку нашей продукции по всей России."
         />
 
         <Advantage
-          img={img3}
+          img={img7}
           heading="Доступные цены"
           description="Так как мы являемся производителем, мы 
                 предлагаем нашу продукцию без лишних наценок."
         />
 
         <Advantage
-          img={img4}
+          img={img8}
           heading="Гарантия качества"
           description="Мы уделяем особое внимание качеству нашей продукции и дорожим нашими клиентами."
         />
